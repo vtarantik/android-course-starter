1d8b416d273797b5d310e758139ecdb5274d8f00
vtarantik <vtarantik@betterpool.com>
2023-08-22 23:07:06 +0200

    initial commit

---
+++ app/src/main/kotlin/com/tarantik/android_course_starter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tarantik.android_course_starter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cz.tarantik.android_course_starter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
